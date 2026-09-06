b1f6e5b25364867f918da431a032d4b6cf9a71ee
hessamashari <hessam.ashari@gmail.com>
2026-09-07 00:45:59 +0330

    second

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://hessamashari.github.io/novand-astro-site,
+  site: 'https://hessamashari.github.io/novand-astro-site',
   base: '/novand-astro-site',
   integrations: [sitemap()],
   vite: {
